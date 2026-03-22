78650bb0b63116128787d9f70c0e0e3017d968e5
raghulsrd <142403016+raghulsrd@users.noreply.github.com>
2026-03-22 10:40:05 +0530

    Add files via upload

---
+++ Web-Glob/glob.js
@@ -0,0 +1,5 @@
+// const sidebarToggle = document.querySelector('.menu-button');
+//     const sidebarClose = document.querySelector('.sidebar li:first-child');
+
+//     sidebarToggle.addEventListener('click', showSidebar);
+//     sidebarClose.addEventListener('click', hideSidebar);
